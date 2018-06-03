80c549a4221b65bdc49c4fe17ec71bade236d168
sloutmyv <clercsyl9123@gmail.com>
2018-06-03 10:55:57 +0200

    6-Rendering HTML

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,5 +3,16 @@ from django.shortcuts import render
 
 # Function based view
 def home(request):
-    return HttpResponse("Hello")
+    html_="""
+    <!doctype html>
+    <html lang="en">
+    <head>
+    </head>
+    <body>
+    <h1>Hello World !</h1>
+    <p>This is html</p>
+    </body>
+    </html>
+    """
+    return HttpResponse(html_)
     #return render(request,'home.html',{})
