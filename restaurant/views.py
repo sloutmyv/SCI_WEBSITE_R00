fa1af51f88ed824c41f1a3675e2acfba92699a06
sloutmyv <clercsyl9123@gmail.com>
2018-06-03 14:18:34 +0200

    7-Render a Django Template

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,18 +1,8 @@
+import random
 from django.http import HttpResponse
 from django.shortcuts import render
 
 # Function based view
 def home(request):
-    html_="""
-    <!doctype html>
-    <html lang="en">
-    <head>
-    </head>
-    <body>
-    <h1>Hello World !</h1>
-    <p>This is html</p>
-    </body>
-    </html>
-    """
-    return HttpResponse(html_)
-    #return render(request,'home.html',{})
+    num = random.randint(0,1000)
+    return render(request,'base.html',{'var':'Context variable', 'num': num})
