dd694b4f56ae93d959fbb8d4a5654c0877e67be4
sloutmyv <clercsyl9123@gmail.com>
2018-06-04 17:43:11 +0200

    13-Template View

--- restaurant/views.py
+++ restaurant/views.py
@@ -3,27 +3,24 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 from django.views import View
+from django.views.generic import TemplateView
 
-# Function based view
-def home(request):
-    num = random.randint(0,1000)
-    some_list = [num, random.randint(0,1000),random.randint(0,1000)]
-    context = {
-        'bool_item':False,
-        'num': num,
-        'some_list':some_list}
-
-    return render(request,'home.html',context)
-
-def about(request):
-    context = {}
-    return render(request,'about.html',context)
-
-def contact(request):
-    context = {}
-    return render(request,'contact.html',context)
-
-class ContactView(View):
-    def get(self, request, *args, **kwargs):
-        context = {}
-        return render(request,'contact.html',context)
+class HomeView(TemplateView):
+    template_name = 'home.html'
+    def get_context_data(self, *args, **kwargs):
+        context = super(HomeView, self).get_context_data(*args, **kwargs)
+        num = None
+        some_list = [
+            random.randint(0,100),
+            random.randint(0,100),
+            random.randint(0,100),
+        ]
+        bool_item = True
+        if bool_item:
+            num = random.randint(0,100)
+        context = {
+            'bool_item':bool_item,
+            'num':num,
+            "some_list":some_list
+        }
+        return context
