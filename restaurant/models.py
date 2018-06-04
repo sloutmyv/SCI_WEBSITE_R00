5d3144e48a5a4ff8d8281415eda318c4213ce4c9
sloutmyv <clercsyl9123@gmail.com>
2018-06-04 18:22:42 +0200

    15-More on Model Fields

--- restaurant/models.py
+++ restaurant/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Restaurant(models.Model):
     name = models.CharField(max_length=120)
     location = models.CharField(max_length=120, null=True, blank=True)
+    category = models.CharField(max_length=120, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
