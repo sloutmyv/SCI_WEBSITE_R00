6fedaedee1993c070c524cbc8f2a0d4bdb221226
sloutmyv <clercsyl9123@gmail.com>
2018-06-27 21:35:22 +0200

    32 - LoginView (bis)

--- sci_website_R00/urls.py
+++ sci_website_R00/urls.py
@@ -17,7 +17,7 @@ from django.conf.urls import url
 from django.contrib import admin
 
 from django.views.generic import TemplateView
-from django.contrib.auth.views import LoginView
+from django.contrib.auth.views import LoginView, PasswordResetView
 
 from restaurant.views import (
 restaurant_listview,
@@ -36,6 +36,7 @@ urlpatterns = [
     url(r'^restaurant/$', RestaurantsListView.as_view()),
     url(r'^restaurant/(?P<slug>[\w-]+)/$', RestaurantDetailView.as_view()),
     url(r'^login/', LoginView.as_view(), name='login'),
+    url(r'^password_reset/', PasswordResetView.as_view(), name='password_reset'),
     #url(r'^restaurant/(?P<slug>\w+)/$', RestaurantsListView.as_view()),
     url(r'^about$', TemplateView.as_view(template_name='about.html')),
     url(r'^contact$', TemplateView.as_view(template_name='contact.html')),
