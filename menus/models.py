2782f0cea4dccd3013efac9cb9756c637689aad8
sloutmyv <clercsyl9123@gmail.com>
2018-06-29 16:05:55 +0200

    34 - Menu Items App

---
+++ menus/models.py
@@ -0,0 +1,25 @@
+from django.conf import settings
+from django.db import models
+
+from restaurant.models import Restaurant
+
+class Item(models.Model):
+    # association
+    user        = models.ForeignKey(settings.AUTH_USER_MODEL)
+    restaurant  = models.ForeignKey(Restaurant)
+    # item stuff
+    name        = models.CharField(max_length=120)
+    contents    = models.TextField(help_text='separate each item by comma')
+    excludes    = models.TextField(blank=True, null=True, help_text='separate each item by comma')
+    public      = models.BooleanField(default=True)
+    timestamp   = models.DateTimeField(auto_now_add=True)
+    updated     = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-updated','-timestamp'] # Item.objects.all()
+
+    def get_contents(self):
+        return self.contents.split(",")
+
+    def get_excludes(self):
+        return self.excludes.split(",")    
