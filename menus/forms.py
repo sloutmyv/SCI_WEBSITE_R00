b95f39831e5d5e5be57fddddcdd13dd917a922bb
sloutmyv <clercsyl9123@gmail.com>
2018-06-30 11:12:35 +0200

    36 - Limiting Form Field to QuerySet

--- menus/forms.py
+++ menus/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from restaurant.models import Restaurant
+
 from .models import Item
 
 class ItemForm(forms.ModelForm):
@@ -12,3 +14,8 @@ class ItemForm(forms.ModelForm):
         'excludes',
         'public',
         ]
+    def __init__(self, user=None, *args, **kwargs):
+        #print(kwargs.pop['user'])
+        print(user)
+        super(ItemForm, self).__init__(*args, **kwargs)
+        self.fields['restaurant'].queryset = Restaurant.objects.filter(user=user) #.exclude (item__isnull=False)
--- menus/views.py
+++ menus/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import ListView, DetailView, CreateView, UpdateView
 
 from .forms import ItemForm
@@ -14,7 +15,7 @@ class ItemDetailView(DetailView):
         queryset = Item.objects.filter(user=self.request.user)
         return queryset
 
-class ItemCreateView(CreateView):
+class ItemCreateView(LoginRequiredMixin, CreateView):
     template_name = 'forms.html'
     form_class = ItemForm
 
@@ -22,6 +23,11 @@ class ItemCreateView(CreateView):
         instance = form.save(commit=False)
         instance.user = self.request.user
         return super(ItemCreateView, self).form_valid(form)
+
+    def get_form_kwargs(self):
+        kwargs = super(ItemCreateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
 
     def get_queryset(self):
         queryset = Item.objects.filter(user=self.request.user)
@@ -32,7 +38,7 @@ class ItemCreateView(CreateView):
         context['title'] = 'Create Item'
         return context
 
-class ItemUpdateView(UpdateView):
+class ItemUpdateView(LoginRequiredMixin, UpdateView):
     template_name = 'forms.html'
     form_class = ItemForm
     def get_queryset(self):
@@ -43,3 +49,8 @@ class ItemUpdateView(UpdateView):
         context = super(ItemUpdateView, self).get_context_data(*args,**kwargs)
         context['title'] = 'Update Item'
         return context
+
+    def get_form_kwargs(self):
+        kwargs = super(ItemUpdateView, self).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
